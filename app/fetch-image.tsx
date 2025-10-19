fa0448cf8d6d1dd40d20f0e57564c7ab7ff6c64b
yuke <yusukesaikyou931@gmail.com>
2025-10-20 03:38:53 +0900

    add Demo env variables

--- app/fetch-image.tsx
+++ app/fetch-image.tsx
@@ -1,13 +1,19 @@
+"use server";
+
 import type { CatImageData } from "./types";
+import { CAT_API_KEY } from "./env";
 
 // APIから画像を取得する関数
 export async function fetchCatImage(): Promise<CatImageData> {
-  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const res = await fetch("https://api.thecatapi.com/v1/images/search", {
+    headers: { "x-api-key": CAT_API_KEY },
+  });
 
   if (!res.ok) {
     throw new Error("画像の取得に失敗しました");
   }
 
   const result = await res.json();
+  console.log("画像を取得しました", result[0]);
   return result[0];
 }
