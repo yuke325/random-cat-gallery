7823d067f59aa9ef8f50d36f587ef177fbd4336f
yuke <yusukesaikyou931@gmail.com>
2025-10-26 05:49:35 +0900

    Add: .env.example

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -14,11 +14,16 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
+// ボタンを押すと猫の画像を取得表示するコンポーネント
 export function CatImage({ images }: { images: CatImageData[] }) {
+  // 表示する猫画像データ
   const [imageData, setImageData] = useState<CatImageData[]>(images);
+  // 取得中のローディング状態
   const [isLoading, setIsLoading] = useState(false);
+  // エラーメッセージ
   const [error, setError] = useState<string | null>(null);
 
+  // 新しい猫の画像を取得しステートを更新、エラーハンドリング&ローディング状態を管理
   const refreshImages = async () => {
     setIsLoading(true);
     setError(null);
@@ -36,9 +41,12 @@ export function CatImage({ images }: { images: CatImageData[] }) {
 
   return (
     <div className="flex flex-col items-center gap-2 pt-4 px-4">
+      {/* ローディング表示 */}
       {isLoading && (
         <p className="text-5xl font-bold text-primary p-40">にゃんこ待ち...</p>
       )}
+
+      {/* 画像カルーセル表示 */}
       {!isLoading && imageData.length > 0 && (
         <Carousel className="w-[80vh]">
           <CarouselContent>
@@ -65,7 +73,11 @@ export function CatImage({ images }: { images: CatImageData[] }) {
           <CarouselNext />
         </Carousel>
       )}
+
+      {/* エラー表示 */}
       {error && <p className="text-5xl font-bold p-10 text-red-500">{error}</p>}
+
+      {/* 画像取得ボタン */}
       <Button
         onClick={refreshImages}
         disabled={isLoading}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { CatImage } from "./cat-image";
 import { fetchCatImages } from "./fetch-image";
 
+// ページの表示と同時に猫の画像を取得して表示する
 export default async function Home() {
   const initialImages = await fetchCatImages();
   return <CatImage images={initialImages} />;
